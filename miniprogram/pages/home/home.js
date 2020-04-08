9fd3275dd7a331b6934351d2ae57a6442ccf477a
liying <liying@izhixin.net>
2020-04-08 22:26:30 +0800

    first commit

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,110 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"hello MINA",
+    num:10000,
+    isGirl:true,
+    person:{
+      name:"张三",
+      age:18,
+      sex:"male"
+    },
+    list:[
+      {
+        name:"张三",
+        age:12,
+        sex:"male",
+        id:1
+      },
+      {
+        name:"李四",
+        age:18,
+        sex:"male",
+        id:2
+      },
+      {
+        name:"王五",
+        age:28,
+        sex:"male",
+        id:3
+      }
+    ],
+    inputValue:0,
+  },
+
+  inputAction(value){
+    console.log(value.detail.value);
+    this.setData({
+      inputValue : value.detail.value
+    })
+  },
+
+  tapAction(e){
+    console.log(e.currentTarget.dataset.tag);
+    this.setData({
+      inputValue : this.data.inputValue+(e.currentTarget.dataset.tag == 1 ? 1 : -1)
+    })
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
